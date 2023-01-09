92ccc1a43d3a22a59e805ac4bd8b652e592678c6
uoch <chaouch.thameurtc61@gmail.com>
2023-01-09 19:51:35 +0100

    first_commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,6 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	node->n = value;
 	node->parent = parent;
 	parent->left = node;
+	parent->right = NULL;
 	node->left = NULL;
 	node->right = NULL;
 	return (node);
