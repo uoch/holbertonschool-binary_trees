d8260c8eedb83c1109c361db12b196b57b2f0f07
uoch <chaouch.thameurtc61@gmail.com>
2023-01-10 21:44:07 +0100

    test

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves- function to calculate the size(adding child)
+ * @tree: pointer to the  tree to check
+ * Return: height of tree or 0 otherwise
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t i = 1;
+
+	if (tree == NULL)
+		return (0);
+	if ((!tree->left && !tree->right) || !tree->parent)
+		i += binary_tree_leaves(tree->right);
+	return (i);
+}
