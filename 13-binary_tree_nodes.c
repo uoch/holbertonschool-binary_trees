c45e644351900e24b7fce3c33a04c621f02386f8
uoch <chaouch.thameurtc61@gmail.com>
2023-01-10 22:30:03 +0100

    countnodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,10 +6,9 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-
 	if (tree == NULL)
 		return (0);
-	if ((!tree->left || !tree->right))
-		return (1);
-	return (binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left) + 1);
+	if ((tree->left || tree->right))
+		return (binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left) + 1);
+	return (0);
 }
