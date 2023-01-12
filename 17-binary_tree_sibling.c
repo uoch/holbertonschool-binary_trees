564bf02c0d4d9c1d84f0199a56a494d1e3847dd2
uoch <chaouch.thameurtc61@gmail.com>
2023-01-12 20:23:15 +0100

    fix_betty_sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,14 +1,22 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_sibling - function to serch for the sibling
+ * @node: pointer to the  node
+ * Return: brothr or sister of the given node
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent) {
-        return NULL;
-    }
+	if (!node || !node->parent)
+	{
+		return (NULL);
+	}
 
-    if (node == node->parent->left) {
-        return node->parent->right;
-    } else {
-        return node->parent->left;
-    }
+	if (node == node->parent->left)
+	{
+		return (node->parent->right);
+	}
+	else
+	{
+		return (node->parent->left);
+	}
 }
