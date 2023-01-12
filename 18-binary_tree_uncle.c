d51bed4342558389345bab5e31f1b644640d52cd
uoch <chaouch.thameurtc61@gmail.com>
2023-01-12 22:18:02 +0100

    test

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_uncle - to fix tomorow
+ * @node: pointer to the  node
+ * Return: brothr or sister of the given node
+ */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node){
 
 	if (!node || !node->parent|| !node->parent->parent)
