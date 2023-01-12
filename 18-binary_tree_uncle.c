971f00fca78742434de6161e06757c3b3e9b6cea
uoch <chaouch.thameurtc61@gmail.com>
2023-01-12 22:38:12 +0100

    node->parent

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,23 +1,21 @@
 #include "binary_trees.h"
 /**
- * binary_tree_uncle - 
+ * binary_tree_uncle - looks for the uncle
  * @node: pointer to the  node
  * Return: return the uncle
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-
-	if (!node || !node->parent || !node->parent->parent)
-	{
-		return (NULL);
-	}
-	if (node->parent == node->parent->parent->left)
-	{
-		return (node->parent->parent->left);
-	}
-	else
-	{
-		return (node->parent->parent->right);
-	}
-	return (NULL);
+    if (!node || !node->parent)
+    {
+        return (NULL);
+    }
+    if (node->parent->parent)
+    {
+        if (node->parent->parent->left && node->parent->parent->left != node->parent)
+            return (node->parent->parent->left);
+        if (node->parent->parent->right && node->parent->parent->right != node->parent)
+            return (node->parent->parent->right);
+    }
+    return (NULL);
 }
